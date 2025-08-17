455d7f8fefd362d04163142603c05d3cdc2d43c8
Sumitasawa <132041180+Sumitasawa@users.noreply.github.com>
2025-08-17 16:38:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded",()=>{
+	const buttons=document.querySelectorAll(".btn");
+	const stopButton=document.querySelector(".stop");
+
+	let cuurentAudio=null;
+	buttons.forEach(button=>{
+		button.addEventListener("click",()=>{
+			const soundFile=button.getAttribute("data-sound");
+
+		 if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+      }
+	currentAudio = new Audio(`sounds/${soundFile}`);
+    currentAudio.play();
+		});
+	});
+
+	 stopButton.addEventListener("click", () => {
+    if (currentAudio) {
+      currentAudio.pause();
+      currentAudio.currentTime = 0;
+      currentAudio = null;
+    }
+  });
+})
